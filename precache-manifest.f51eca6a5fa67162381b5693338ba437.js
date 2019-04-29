1118ed18e081d31d6d08d8ab03b8ac4d65d853cd
Alex <alex.reardon14@gmail.com>
2019-04-29 17:47:57 -0400

    Updates

---
+++ precache-manifest.f51eca6a5fa67162381b5693338ba437.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7093ead30be0a6c5ca6e",
+    "url": "/baby-names/static/js/runtime~main.7093ead3.js"
+  },
+  {
+    "revision": "eb4b3663981ee8499922",
+    "url": "/baby-names/static/js/main.eb4b3663.chunk.js"
+  },
+  {
+    "revision": "c99f7809d90e0b141863",
+    "url": "/baby-names/static/js/1.c99f7809.chunk.js"
+  },
+  {
+    "revision": "eb4b3663981ee8499922",
+    "url": "/baby-names/static/css/main.9ca950e2.chunk.css"
+  },
+  {
+    "revision": "207ab09a38bdec3be788c8555415679f",
+    "url": "/baby-names/index.html"
+  }
+];
